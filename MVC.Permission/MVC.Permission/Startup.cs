223aca9bc0d88998f2e267d794a2973fabf8c264
vinniefeng <fcq121@163.com>
2016-08-11 15:25:20 +0800

    .

---
+++ MVC.Permission/MVC.Permission/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.Permission.Startup))]
+namespace MVC.Permission
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
